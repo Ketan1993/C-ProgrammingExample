6cf80f0117c170fdbda8f641bb11a0e2df3fa73a
ketan <ketan@wimerasys.com>
2023-08-31 14:49:52 +0530

    Arry example added

--- ArrayExample/main.c
+++ ArrayExample/main.c
@@ -1,4 +1,19 @@
+#include <stdio.h>
 #include "array.h"
+
+// common array print function
+// pass array
+// pass length of array
+static void print_array_(int a[], int size);
+
+static void print_array_(int a[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+}
 
 int main()
 {
@@ -6,16 +21,18 @@ int main()
 	int a_size = sizeof(a) / sizeof(a[0]);
 
 #ifdef ARRAY_SORT
-	int ret = sort_arry_in_aascending_order(a, a_size);
+
+	printf("before sort array \n");
+	print_array_(a, a_size);
+
+	int ret = sort_arry_in_ascending_order(a, a_size);
 
 	if (ret == 1)
 	{
-		for (int i = 0; i < a_size; i++)
-		{
-			printf("%d ", a[i]);
-		}
-	}
+		printf("ascending order array \n");
+		print_array_(a, a_size);
 #endif // ARRAY_SORT
 
-	return 0;
+		return 0;
+	}
 }
